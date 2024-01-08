7e1b9f1026c44e6d8ca9f6e43e79dde73a6d80dd
deffjaniels <107448750+deffjaniels@users.noreply.github.com>
2024-01-08 13:43:39 -0500

    v 0.1

---
+++ EncounterArrays.js
@@ -0,0 +1,94 @@
+let mediumEncountersMjs = [
+  [opponentMonsters.blockgym1],
+  [opponentMonsters.strengthgym1],
+  [opponentMonsters.balancegym1],
+  [opponentMonsters.healgym1],
+  [easyMultiEncounters.e4, easyMultiEncounters.e5, easyMultiEncounters.e4],
+  [opponentMonsters.strengthgym1, opponentMonsters.strengthgymguard],
+
+  // {
+  //   opponents: [opponentMonsters.healgymguard2, opponentMonsters.blockgym1],
+  //   goldReward: 35,
+  //   XP: 20
+  // },
+  // {
+  //   opponents: [opponentMonsters.strengthgymguard, opponentMonsters.blockgym1],
+  //   goldReward: 35,
+  //   XP: 20
+  // },
+  // {
+  //   opponents: [opponentMonsters.strengthgymguard, opponentMonsters.healgym1],
+  //   goldReward: 35,
+  //   XP: 20
+  // },
+
+  // {
+  //   opponents: [opponentMonsters.healgymguard2, opponentMonsters.strengthgym1],
+  //   goldReward: 35,
+  //   XP: 20
+  // },
+]
+
+let easyEncounters = [
+  [easySoloEncounters.e1],
+  [easySoloEncounters.e2],
+  [easySoloEncounters.e3],
+  [easySoloEncounters.e4],
+  [easySoloEncounters.e5],
+
+  [easySoloEncounters.e6],
+  [easySoloEncounters.e7],
+  [easySoloEncounters.e8],
+  [easyMultiEncounters.em1, easyMultiEncounters.em2],
+  [easyMultiEncounters.em3, easyMultiEncounters.em4],
+
+  [easyMultiEncounters.em2, easyMultiEncounters.em4],
+]
+
+let mediumEncounters = [
+  [mediumSoloEncounters.m1],
+  [mediumSoloEncounters.m2],
+  [mediumSoloEncounters.m3],
+  [mediumSoloEncounters.m4],
+  [mediumSoloEncounters.m5],
+
+  [mediumSoloEncounters.m6],
+  [mediumSoloEncounters.m7],
+  [mediumSoloEncounters.m8],
+  [mediumSoloEncounters.m9],
+  [mediumSoloEncounters.m10],
+
+  [mediumMultiEncounters.mm1, mediumMultiEncounters.mm2],
+  [mediumMultiEncounters.mm1, mediumMultiEncounters.mm3],
+  [mediumMultiEncounters.mm2, mediumMultiEncounters.mm3],
+  [mediumMultiEncounters.mm1, mediumMultiEncounters.mm4],
+  [mediumMultiEncounters.mm3, mediumMultiEncounters.mm4],
+
+  [easyMultiEncounters.em1, easyMultiEncounters.em2, easyMultiEncounters.em1],
+]
+
+let hardEncounters = [
+  [hardSoloEncounters.h1],
+  [hardSoloEncounters.h2],
+  [hardSoloEncounters.h3],
+  [hardSoloEncounters.h4],
+  [hardSoloEncounters.h5],
+
+  [hardSoloEncounters.h6],
+  [hardSoloEncounters.h7],
+  [hardSoloEncounters.h8],
+  [hardSoloEncounters.h9],
+  // [easySoloEncounters.e6, easySoloEncounters.e2],
+  // [easySoloEncounters.e6, easySoloEncounters.e7],
+  // [easySoloEncounters.e2, easySoloEncounters.e7],
+]
+
+
+let bosses = [
+    [bossMonsters.boss1],
+    [bossMonsters.boss2],
+    [bossMonsters.boss3],
+    [bossMonsters.boss4],
+    [bossMonsters.boss5],
+    [bossMonsters.healgymboss, bossMonsters.healgymguard2],
+]
